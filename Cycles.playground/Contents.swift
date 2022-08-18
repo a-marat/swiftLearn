28c9e80b8691c45a59df7ea16d0182743630dcc8
a-marat <abdumarat96@gmail.com>
2022-08-18 22:58:25 +0300

    Изучение работы циклов while и repeat while

--- Cycles.playground/Contents.swift
+++ Cycles.playground/Contents.swift
@@ -27,3 +27,57 @@ let minutes = 60
 for tickMark in 0..<minutes {
     print(tickMark)
 }
+
+
+/* Цикл while */
+
+/*
+
+ while условие {
+    инструкици
+ }
+ 
+ */
+
+var number = 1
+
+while number <= 10 {
+    print(number)
+    number += 1
+}
+print("Start")
+
+
+/* Цикл repear while */
+
+/*
+
+ repeat {
+    инструкции
+ } while условие
+ 
+*/
+
+var number2 = 1
+
+repeat {
+    print(number2)
+    number2 += 1
+} while number2 <= 5
+
+print("Finish")
+
+/* Break */
+
+var countDown = 10
+
+while countDown >= 0 {
+    print(countDown)
+    
+    if countDown == 5 {
+        print("Yes")
+        break
+    }
+    
+    countDown -= 1
+}
