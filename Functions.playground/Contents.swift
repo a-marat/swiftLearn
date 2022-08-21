525edb5dc74c1d2776f4a7b583c81ca7057d0557
a-marat <abdumarat96@gmail.com>
2022-08-21 23:15:54 +0300

    Изучение работы функций ч.2

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -49,3 +49,57 @@ func getUser2() -> (first: String, last: String) {
 }
 let user2 = getUser2()
 print(user2.first)
+
+
+// Имена параметров
+
+func sayHello(to name: String) { // Задаем внешний (to) и внешний параметры (name)
+    print("Hello, \(name)")
+}
+sayHello(to: "Marat")
+
+
+// Пропуск имен параметров
+
+func greet(_ person: String) { // Внешнее имя параметра функции можно пропустить символом нижнего подчеркивания "_"
+    print("Hello, \(person)")
+}
+greet("Arslan")
+
+
+// Параметры по умолчанию
+
+func greet1(_ person: String, nicely: Bool = true) {
+    
+    if nicely == true {
+        print("Hello, \(person)")
+    } else {
+        print("Oh no^ it's \(person) again")
+    }
+}
+
+//greet1("Arkadiy") // Вызов функции без параметров по умолчанию. Выведет Hello Arkadiy
+
+greet1("Arkadiy", nicely: false) // Вызов функции с параметрами по умолчанию. Выведет Oh no^ it's Arkadiy again
+
+
+// Вариативные параметры и вариативные функции
+
+func square2(numbers: Int...) { // ... после типа параметра позволяет сделать тип и функцию в последствии вариативными
+    for number in numbers {
+        print("\(number) в квадрате будет \(number * number)")
+    }
+}
+square2(numbers: 1, 2, 3, 4, 5)
+
+
+// Сквозные параметры
+
+func doubleIt(number: inout Int) { // С помощью ключевого слова inout можно сделать параметр сквозным, чтобы сменить его тип
+    number *= 2
+}
+
+var myNumber = 5
+
+doubleIt(number: &myNumber)
+
