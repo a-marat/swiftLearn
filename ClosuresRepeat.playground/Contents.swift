325c7dc8b026c6617d099d5db0f8bc1da73358fc
a-marat <abdumarat96@gmail.com>
2022-08-29 22:11:07 +0300

    Повтор изучения замыканий. ч2

--- ClosuresRepeat.playground/Contents.swift
+++ ClosuresRepeat.playground/Contents.swift
@@ -47,3 +47,85 @@ let drivingWithReturn = { (place: String) -> String in
 
 let message = drivingWithReturn("London")
 print(message) // Выведет I'm going to London in my car
+
+
+// Создание пустого клоужера
+let driving3 = {
+    print("I'm driving to go")
+}
+
+// Создание фукнции где в качестве параметров прописываем синтаксис пустого клоужера
+func travel(action: () -> Void) {
+    print("I'm getting ready to go")
+    action()
+    print("I arrived!")
+}
+
+travel(action: driving3) // Вызов фукнции с пустым клоужером
+
+// Вызов функции с клоужером. Данный метод работает только если последним параметром функции является пусто клоужер.
+travel() {
+    print("I'm driving in my car")
+}
+
+
+// Клоужеры, которые принимают параметры и сами являются параметрами функции
+
+func travel2(action: (String) -> Void) {
+    print("I'm getting ready to go")
+    action("London")
+    print("I arrived!")
+}
+
+travel2 { (place: String) in
+    print("I'm going to \(place) in my car")
+}
+
+
+//
+
+let changeSpeed = { (speed: Int) in
+    print("Changing speed to \(speed)kph")
+}
+
+func buildCar(name: String, engine: (Int) -> Void) {
+    // build the car
+}
+
+func travel3(action: (String) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London")
+    print(description)
+    print("I arrived!")
+}
+
+travel3 { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
+
+
+// Сокращенные имена параметров
+
+func travel4(action: (String, Int) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London", 60)
+    print(description)
+    print("I arrived!")
+}
+
+travel4 {
+    "I'm going to \($0) at \($1) miles per hour"
+}
+
+
+// Возвращение клоужера из функции
+
+func travel4() -> (String) -> Void {
+    return {
+        print("I'm going to \($0)")
+    }
+}
+
+let result = travel4()
+
+result("London")
