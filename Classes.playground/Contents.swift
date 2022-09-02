2c19a3977abee526da05f570300f2314f2f3f874
a-marat <abdumarat96@gmail.com>
2022-09-02 23:26:34 +0300

    Изучение работы классов. ч. 2. Вычисляемые и хранимые свойства.

--- Classes.playground/Contents.swift
+++ Classes.playground/Contents.swift
@@ -73,3 +73,101 @@ class HumanThree {
 var arslan = HumanThree(age: 26, name: "Арслан", isQualified: true)
 arslan.status // Выведет Арслан подходит для этой работы
 
+// Вычисляемые свойства
+
+/* Структура вычисляемых свойств
+ 
+ var имя свойства: тип данных {
+    get {
+        // вычисление значения
+    }
+    set (параметр) {
+        // установка значения
+    }
+ }
+ 
+*/
+
+
+class Account {
+    
+    var sum: Double = 0 // сумма вклада
+    var rate: Double = 0.01 // процентная ставки
+    
+    var profit: Double {
+        
+        get {
+            return sum + sum * rate
+        }
+        set (newProfit) {
+            self.sum = newProfit / (1 + rate)
+        }
+    }
+    
+    init(sum: Double, rate: Double) {
+        self.sum = sum
+        self.rate = rate
+    }
+}
+
+var myAcc: Account = Account(sum: 1000, rate: 0.1)
+
+print(myAcc.profit)
+
+myAcc.profit = 2000 // ожидаемая прибыль
+
+print(myAcc.sum) // необходимая сумма вклада для получения этой прибыли
+
+
+// Хранимые свойства
+
+/* Структура хранимых свойств
+ 
+ var имя свойства: тип данных {
+    
+    willSet(параметр) {
+    // выражения
+    }
+    didSet(параметр) {
+    // выражения
+    }
+ }
+ 
+*/
+
+
+class Account2 {
+    
+    var sum: Double {
+        willSet(newSum) {
+            print("Предыдущая сумма \(self.sum) / Новая сумма \(newSum)")
+        }
+        didSet(oldSum) {
+            print("Сумма увеличилась на: \(self.sum - oldSum)")
+        }
+    } // сумма вклада
+    var rate: Double = 0.01 // процентная ставки
+    
+    var profit: Double {
+        
+        get {
+            return sum + sum * rate
+        }
+        set (newProfit) {
+            self.sum = newProfit / (1 + rate)
+        }
+    }
+    
+    init(sum: Double, rate: Double) {
+        self.sum = sum
+        self.rate = rate
+    }
+}
+
+var myAcc2: Account2 = Account2(sum: 1000, rate: 0.1)
+
+print(myAcc2.profit)
+
+myAcc2.profit = 2000 // ожидаемая прибыль
+
+print(myAcc2.sum) // необходимая сумма вклада для получения этой прибыли
